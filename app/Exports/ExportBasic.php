1391f851c10224e8f405113b0fd13e90ee3cefd2
jeeno44 <jeep65418@gmail.com>
2022-06-18 21:22:46 +0300

    Commit 18.06.2022 21:22

--- app/Exports/ExportBasic.php
+++ app/Exports/ExportBasic.php
@@ -4,6 +4,9 @@ namespace App\Exports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+
 
 class ExportBasic implements FromCollection
 {
