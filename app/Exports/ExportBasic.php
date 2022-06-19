42f14425cf344033b01b50ac6b2bb06b4ae1bb14
jeeno44 <jeep65418@gmail.com>
2022-06-19 19:15:42 +0300

    Commit 19.06.2022 19:15

--- app/Exports/ExportBasic.php
+++ app/Exports/ExportBasic.php
@@ -3,18 +3,21 @@
 namespace App\Exports;
 
 use App\Models\User;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 
-class ExportBasic implements FromCollection,WithHeadingRow
+class ExportBasic implements FromQuery,WithHeadingRow
 {
     /**
     * @return \Illuminate\Support\Collection
     */
 
+    use Exportable;
 
     public function query()
     {
