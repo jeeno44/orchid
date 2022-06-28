90f2cf5457eb1394732680ad84c0e55c138d5596
jeeno44 <jeep65418@gmail.com>
2022-06-28 09:32:09 +0300

    Commit 28.06.2022 09:32

--- app/Mail/SendEmail.php
+++ app/Mail/SendEmail.php
@@ -28,7 +28,7 @@ class SendEmail extends Mailable
      */
     public function build()
     {
-        return $this->to(['jeep456@yandex.ru'])->text("Somebody else");
+        return $this->to(['jeep456@yandex.ru'])->text("email.some");
         //return $this->view('view.name');
     }
 }
