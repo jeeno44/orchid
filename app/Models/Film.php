fcf256c2988fa3539c73d1febff5e36b009b7d5d
jeeno44 <jeep65418@gmail.com>
2022-09-12 21:48:47 +0300

    Маленько поправил фильмы и сериалы

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\AsSource;
 
 class Film extends Model
 {
     use HasFactory;
+    use AsSource;
+
+    protected $fillable = ['name','year','type','watched'];
 }
