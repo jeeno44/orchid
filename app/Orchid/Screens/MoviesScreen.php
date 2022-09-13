d90ccd2bc6191a234213d64856fcd190cfc123ee
jeeno44 <jeep65418@gmail.com>
2022-09-13 20:32:12 +0300

    Правка фильмов

--- app/Orchid/Screens/MoviesScreen.php
+++ app/Orchid/Screens/MoviesScreen.php
@@ -3,7 +3,11 @@
 namespace App\Orchid\Screens;
 
 use App\Models\Film;
+use Illuminate\Support\Facades\Request;
 use Orchid\Screen\Actions\ModalToggle;
+use Orchid\Screen\Fields\DateTimer;
+use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Fields\Select;
 use Orchid\Screen\Screen;
 use Orchid\Screen\TD;
 use Orchid\Support\Facades\Layout;
@@ -41,7 +45,7 @@ class MoviesScreen extends Screen
     {
         return [
             //ModalToggle::make("Редактировать задание")->modal("editTask")->method("edittask"),
-            //ModalToggle::make("Добавить задание")->modal("appendTask")->method("settask"),
+            ModalToggle::make("Добавить фильм или сериал")->modal("appendMovie")->method("setmovie"),
         ];
     }
 
@@ -54,14 +58,37 @@ class MoviesScreen extends Screen
     {
         return [
             Layout::table("films",[
-                TD::make("id"),
+                TD::make("id")->width(50),
                 TD::make("name","Имя"),
                 TD::make("year","Год"),
-                TD::make("type","Тип"),
+                TD::make("type","Тип")->render(function (Film $film){
+                    return $film->type === "movie" ? "Фильм" : "Сериал";
+                }),
                 TD::make("watched","Просмотрено")->render(function (Film $film){
                     return $film->watched === 0 ? "Не просмотрено" : "Просмотрено";
                 })
-            ])
+            ]),
+            Layout::modal("appendMovie",Layout::rows([
+                Input::make('film')->required()->type("text")->title('Имя'),
+                Input::make('year')->required()->type("text")->title('Год'),
+                Select::make("type")->options([
+                    'movie' => 'Фильм',
+                    'tv_show' => 'Сериал',
+                ]),
+                Select::make("watched")->options([
+                    '1' => 'Посмотрено',
+                    '0' => 'Не посмотрено',
+                ]),
+            ]))->title("Создание задания")->applyButton("Добавить")->closeButton("Отмена"),
         ];
+    }
+
+    public function setmovie (Request $request)
+    {
+        echo "setmovie";
+        dd($request->all());
+        Film::create($request->all());
+        
+        //return view("setmovie",compact(""));
     }
 }
