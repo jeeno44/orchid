2b3931cb3c667595173aed5e9fb657ab7978b900
jeeno44 <jeep65418@gmail.com>
2022-09-29 21:46:55 +0300

    REDIS

---
+++ app/Console/Commands/SendBackupDatabaseMoviesCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class SendBackupDatabaseMoviesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:back';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Делаем Бэкап Базы данных (таблица фильмов)';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Redis::set("name","SWIFT");
+
+        return 0;
+    }
+}
