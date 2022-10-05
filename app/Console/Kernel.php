0ba529d4d83157b7beeadb2f8c455f857784301a
jeeno44 <jeep65418@gmail.com>
2022-10-05 21:39:08 +0300

    Commit 05.10.2022 21:39

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
          $schedule->command('send:tg')->everyMinute();
+         $schedule->command('database:back')->everyMinute();
     }
 
     /**
