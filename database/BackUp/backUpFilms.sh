df511eb32c0fd75fdbdce871ef83b1d7b6972312
jeeno44 <jeep65418@gmail.com>
2022-10-22 10:49:55 +0300

    Commit 22.10.2022 10:49

---
+++ database/BackUp/backUpFilms.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "WELCOME";
+
+# echo "" | mutt -s "Добавлен фильм или сериал" jeep456@yandex.ru -a /home/jeeno/sites/orchid/database/BackUp/films_
+echo "Резервная копия спика фильмов и сериалов" | mutt -s "Добавлен фильм или сериал" jeep456@yandex.ru
