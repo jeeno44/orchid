c24ec8cd488a80fc5e57b3619f9c8b4872d0c381
jeeno <jeep456@yandex.ru>
2022-10-22 11:23:54 +0300

    Backupbase

--- database/BackUp/backUpFilms.sh
+++ database/BackUp/backUpFilms.sh
@@ -8,6 +8,7 @@ file="/home/jeeno/sites/orchid/public/films_2022"*
 if [ -f $file ];
 then
     echo "Файл существует";
+    echo "Резервная копия спика фильмов и сериалов" | mutt -s "Добавлен фильм или сериал" jeep456@yandex.ru -a "$file"
 else
     echo "НЕТ ФАЙЛА";
 fi
