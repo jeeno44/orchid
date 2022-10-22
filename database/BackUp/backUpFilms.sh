4365ab5e6eb48c78249b06cf19e3e656ec5f3282
jeeno44 <jeep65418@gmail.com>
2022-10-22 11:30:42 +0300

    Commit 22.10.2022 11:30

--- database/BackUp/backUpFilms.sh
+++ database/BackUp/backUpFilms.sh
@@ -8,7 +8,8 @@ file="/home/jeeno/sites/orchid/public/films_2022"*
 if [ -f $file ];
 then
     echo "Файл существует";
-    echo "Резервная копия спика фильмов и сериалов" | mutt -s "Добавлен фильм или сериал" jeep456@yandex.ru -a "$file"
+    echo "Резервная копия спика фильмов и сериалов" | mutt -s "Добавлен фильм или сериал" jeep456@yandex.ru -a "$file";
+    mv $file "/home/jeeno/sites/orchid/database/BackUp/done/"
 else
     echo "НЕТ ФАЙЛА";
 fi
