b89cc85c0c594ed4e76b48c52fda4d5c4e0a2c30
jeeno44 <jeep65418@gmail.com>
2022-12-28 15:06:38 +0300

    Commit 28.12.2022 15:06

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,16 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
+use Orchid\Alert\Alert;
 
 class TaskController extends Controller
 {
 
     public function deltask ($id)
     {
+        \alert("DONE");
+
         Task::where([
             ["id",$id],
             ["status","done"]
         ])->delete();
+
+
 
         return redirect(URL::previous());
         //return redirect()->route("platform.tasks");
