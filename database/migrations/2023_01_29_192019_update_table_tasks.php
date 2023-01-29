d15407e9a6ac615437f83eee62a4f848832c446e
kali <kali@mail.ru>
2023-01-29 11:36:16 -0500

    Append CheckBox

--- database/migrations/2023_01_29_192019_update_table_tasks.php
+++ database/migrations/2023_01_29_192019_update_table_tasks.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string("repeat")->after("datetime");
+            $table->string("repeat")->nullable(true)->after("datetime");
         });
     }
 
